70a51d2937582c5bd449420091d154a3dd5a9d66
ADoyle <adoyle.h@gmail.com>
2020-03-22 03:12:09 +0800

    update vagrant/config.rb

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -15,11 +15,13 @@ $subnet = "10.0.20"
 $network_plugin = "flannel"
 $inventory = "inventory/wh_local"
 $shared_folders = {
-  "#{offline_res_path}/base_rpms" => "/var/cache/yum/x86_64/7/offline/base/packages",
-  "#{offline_res_path}/docker_rpms" => "/var/cache/yum/x86_64/7/offline/docker-ce/packages",
-  "#{offline_res_path}/extras_rpms" => "/var/cache/yum/x86_64/7/offline/extras/packages",
-  "#{offline_res_path}/updates_rpms" => "/var/cache/yum/x86_64/7/offline/updates/packages",
-  "#{offline_res_path}/createrepo_rpms" => "/var/cache/yum/x86_64/7/offline/createrepo/packages",
+  "#{offline_res_path}/base_rpms" => "/var/lib/yum_repo/x86_64/7/offline/base/packages",
+  "#{offline_res_path}/docker_rpms" => "/var/lib/yum_repo/x86_64/7/offline/docker-ce/packages",
+  "#{offline_res_path}/extras_rpms" => "/var/lib/yum_repo/x86_64/7/offline/extras/packages",
+  "#{offline_res_path}/updates_rpms" => "/var/lib/yum_repo/x86_64/7/offline/updates/packages",
+  "#{offline_res_path}/createrepo_rpms" => "/var/lib/yum_repo/x86_64/7/offline/createrepo/packages",
+
+  # "#{offline_res_path}/docker_rpms" => "/var/lib/yum_repo/x86_64/7/docker-ce/packages",
 }
 
 $download_cache_dir = File.join(project_abs_dir, offline_res_path, "kubespray_cache")
